d3be7a9f787502ce0fa6c6284f797082ebb1ea19
admin <admin@DESKTOP-DIIK3ID>
2025-08-08 18:15:08 +0700

    check-thing

--- Angular-Project/Degital_Project_FE/src/app/features/client/header/header.component.ts
+++ Angular-Project/Degital_Project_FE/src/app/features/client/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 
-
 @Component({
   selector: 'app-header',
   standalone: false,
@@ -10,64 +9,26 @@ import { AuthService } from '../../../core/services/auth.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
-  constructor(private router: Router, private auth: AuthService) { }
-statusClass = 'hr-t';
-hrLeft: number = 30;
+  constructor(private router: Router) {}
+
+  activeTab: string = 'main';
+
   ngOnInit(): void {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         const url = event.urlAfterRedirects;
-
         if (url === '/' || url.startsWith('/home')) {
-          this.hrLeft = 30;
+          this.activeTab = 'main';
         } else if (url.startsWith('/gallery')) {
-          this.hrLeft = 100;
+          this.activeTab = 'gallery';
         } else if (url.startsWith('/ourproject')) {
-          this.hrLeft = 200;
+          this.activeTab = 'ourproject';
         } else if (url.startsWith('/company')) {
-          this.hrLeft = 300;
+          this.activeTab = 'company';
         } else if (url.startsWith('/contact')) {
-          this.hrLeft = 400;
+          this.activeTab = 'contact';
         }
       }
     });
   }
-
-  goToHome() {
-    this.router.navigate(['homepage']);
-  }
-
-  logout() {
-    this.auth.logout();
-  }
-
-  // statusClass = 'hr-t';
-
-  // setActiveClass() {
-  //   this.statusClass = 'hr-t-change1';
-  // }
-  // setActiveClass2() {
-  //   this.statusClass = 'hr-t-change2';
-  // }
-  // setActiveClass3() {
-  //   this.statusClass = 'hr-t-change3';
-  // }
-  //  setActiveClass4() {
-  //   this.statusClass = 'hr-t-change4';
-  // }
-
-
-
-
-  // bntStyle1: string | undefined;
-  //   AppComponent() {
-
-  //    this. bntStyle1 = 'hr-t';
-  //   }
-  //   submit() {
-  //     this.bntStyle1 = 'hr-t-change1';
-
-  //   }
-
 }
-
